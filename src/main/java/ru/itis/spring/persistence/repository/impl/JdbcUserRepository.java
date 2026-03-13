cf5cbc75962bb3de184a963599ce3c1cd50a31b6
wadrodrog <135006964+wadrodrog@users.noreply.github.com>
2026-03-13 22:56:03 +0300

    NamedParameterJdbcTemplate

--- src/main/java/ru/itis/spring/persistence/repository/impl/JdbcUserRepository.java
+++ src/main/java/ru/itis/spring/persistence/repository/impl/JdbcUserRepository.java
@@ -2,34 +2,39 @@ package ru.itis.spring.persistence.repository.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.itis.spring.persistence.entity.UserEntity;
 import ru.itis.spring.persistence.repository.UserRepository;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class JdbcUserRepository implements UserRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void save(UserEntity user) {
-        String query = "INSERT INTO account (name, birth_date) VALUES (?, ?)";
-        jdbcTemplate.update(query, user.getName(), user.getBirthDate());
+        String query = "INSERT INTO account (name, birth_date) VALUES (:name, :birth_date)";
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("name", user.getName())
+                .addValue("birth_date", user.getBirthDate());
+        jdbcTemplate.update(query, params);
     }
 
     @Override
     public Optional<UserEntity> getById(Long id) {
-        String query = "SELECT * FROM account WHERE id = ?";
+        String query = "SELECT * FROM account WHERE id = :id";
         try {
             UserEntity entity = jdbcTemplate.queryForObject(query,
-                    new BeanPropertyRowMapper<>(UserEntity.class), id);
+                    Collections.singletonMap("id", id),
+                    userMapper);
             return Optional.ofNullable(entity);
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
@@ -44,8 +49,12 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public void update(UserEntity user) {
-        String query = "UPDATE account SET name = ?, birth_date = ? WHERE id = ?";
-        jdbcTemplate.update(query, user.getName(), user.getBirthDate(), user.getId());
+        String query = "UPDATE account SET name = :name, birth_date = :birth_date WHERE id = :id";
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("name", user.getName())
+                .addValue("birth_date", user.getBirthDate())
+                .addValue("id", user.getId());
+        jdbcTemplate.update(query, params);
     }
 
     @Override
